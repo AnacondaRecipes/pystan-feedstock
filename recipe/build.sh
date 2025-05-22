612e884443ea7504dab9f05ba767167a92403809
Steven <scrass@anaconda.com>
2025-05-22 15:19:42 -0600

    test

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
-set -ex
+# #!/bin/bash
+# set -ex
 
-STAN_VER=${PKG_VERSION:0:${#PKG_VERSION}-2}
+# STAN_VER=${PKG_VERSION:0:${#PKG_VERSION}-2}
 
-${PYTHON} -m pip install . -vv --no-deps --no-build-isolation
+# ${PYTHON} -m pip install . -vv --no-deps --no-build-isolation
