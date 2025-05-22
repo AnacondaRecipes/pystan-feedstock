8dc388059ce7fed1938e7395ae6f705177ba903c
Steven <scrass@anaconda.com>
2025-05-22 11:17:37 -0600

    edit bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,18 +1,17 @@
 set "STAN_VER=%PKG_VERSION:~0,-2%"
 
-pushd pystan\\stan\\lib\\stan_math
+pushd stan\lib\stan_math
 if errorlevel 1 exit 1
 
-rd /q /s doc doxygen make test lib\\cpplist_4.45 lib\\gtest_1.7.0
+rd /q /s doc doxygen make test lib\cpplist_4.45 lib\gtest_1.7.0
 if errorlevel 1 exit 1
 
 popd
 if errorlevel 1 exit 1
 
-
 :: For VS<10 copy stdint to the pystan directory so models can be compiled.
 if %VS_MAJOR% LSS 10 (
-  robocopy %LIBRARY_INC% pystan\\stan\\src stdint.h
+  robocopy %LIBRARY_INC% stan\src stdint.h
   if errorlevel GTR 1 exit 1
 )
 
